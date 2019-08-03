7d084c8f0025bf1946043fca3299147553823812
goi <kakedashiit@gmail.com>
2019-08-03 19:02:34 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,25 +2,107 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Card</h1>
+        <CardList />
       </header>
     </div>
   );
 }
+
+
+class CardList extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      card_check: '0',
+      card_value: '',
+      select_card: [],
+      check_cards: ['1','2','3','4','5','6','7']
+    }
+  }
+
+  // shuffle numbering...
+  componentWillMount(){
+    this.shuffleCards();
+  }
+
+  shuffleCards = () => {
+    const array = Object.assign([], this.state.check_cards);
+    for(var i = array.length - 1; i > 0; i--){
+      var r = Math.floor(Math.random() * (i + 1));
+      var tmp = array[i];
+      array[i] = array[r];
+      array[r] = tmp;
+    }
+    this.setState({ check_cards: array});
+  }
+
+  handleOnClick = (e, index) => {
+
+    this.setState({card_value: e});
+    const card = Object.assign([], this.state.select_card);
+    card.push(e);
+    this.setState({ select_card: card });
+    this.openCheck(e, index);
+  }
+
+  openCheck = (card, index) => {
+    let openedCard = this.state.select_card
+    if(openedCard.indexOf(card) != '-1') {
+      alert("Out");
+      this.setState({ card_value: '0' });
+      this.setState({ select_card: [] });
+      this.setState({ check_cards:  ['1','2','3','4','5','6','7'] });
+    }else{
+      const safe_cards = Object.assign([], this.state.check_cards);
+      safe_cards[index - 1] = "*";
+      this.setState({ check_cards: safe_cards });
+      this.cardBlock(card, index);
+    }
+  }
+
+  cardBlock = (card, index) => {
+    let selectItem = document.getElementById("CardList").getElementsByTagName("button");
+    for(let i = 0; i < selectItem.length; i++){
+      selectItem[i].disabled = true;
+    }
+    selectItem[card - 1].disabled = false;
+  }
+
+  render() {
+    return (
+      <div class="">
+        <div id="CardList" class="card_list">
+          <CardSet cardClick={this.handleOnClick} cardList={this.state.check_cards} />
+        </div>
+        <p>{this.state.check_cards}</p>
+      </div>
+    );
+  }
+}
+
+const CardSet = (props) => {
+  
+  const card_li = props.cardList;
+  //const card_li = ['1','2','3','4','5','6','7']
+  const rows = card_li.map((card,index) =>
+    <ul id="CardList" key={card}>
+      <li>
+      <button  onClick={() => props.cardClick(card, index)}>
+          {card}
+      </button>
+      </li>
+    </ul>
+  );
+  return (
+    rows
+  )
+}
+
 
 export default App;
